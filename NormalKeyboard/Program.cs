14ac2c2427dd3aa1306cd61b7479322bbd1d367b
Ruper-frog <ruepr120406@gmail.com>
2023-02-12 22:39:14 +0200

    Added a  new branch

--- NormalKeyboard/Program.cs
+++ NormalKeyboard/Program.cs
@@ -1,16 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection.Emit;
+using System.Windows;
+using System.Windows.Input;
+using System.Xml.Schema;
 
 namespace NormalKeyboard
 {
     internal class Program
     {
+        static void RunKeyboardClass(ref string readLine, int x_Axis, int y_Axis)
+        {
+            Keyboard KeyboardClass = new Keyboard(readLine, x_Axis, y_Axis);
+            KeyboardClass.Call(ref readLine);
+        }
         static void Main(string[] args)
         {
+            string str = "";
 
+            int x = Console.CursorLeft;
+            int y = Console.CursorTop;
+
+            RunKeyboardClass(ref str, x, y);
+
+            Console.WriteLine();
+
+            Console.WriteLine(str);
         }
+        //Console.CursorVisible = false;
     }
 }
